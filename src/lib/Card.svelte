ed249b59ea2dd2ac31b96586cf2c69179a212096
samfundev <samdinner@hotmail.com>
2026-04-11 21:21:41 -0400

    Optimize images

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -3,6 +3,7 @@
 	import { increment, reset } from '$lib/test.svelte';
 	import { fade } from 'svelte/transition';
 	import Popover from './Popover.svelte';
+	import type { Picture } from '@sveltejs/enhanced-img';
 
 	let id = increment('card');
 
@@ -14,6 +15,15 @@
 	}
 
 	const { title, image, href, description }: Props = $props();
+
+	const imageModules: Record<string, { default: Picture }> = import.meta.glob(
+		'/src/lib/assets/*.png',
+		{
+			eager: true,
+			query: { enhanced: true }
+		}
+	);
+	const imageModule = $derived(image ? imageModules[`/src/lib/assets/${image}`] : null);
 </script>
 
 <!-- <Popover>
@@ -43,8 +53,16 @@
 	class="group flex flex-col items-center overflow-hidden rounded-lg border no-underline transition-colors hover:bg-gray-200"
 >
 	{#if image}
-		<div class="relative min-w-full grow overflow-hidden flex items-center justify-center">
-			<img src={image} alt={title} class="aspect-square w-full object-cover" />
+		<div class="relative flex min-w-full grow items-center justify-center overflow-hidden">
+			{#if imageModule}
+				<enhanced:img
+					src={imageModule?.default}
+					alt={title}
+					class="aspect-square w-full object-cover"
+				/>
+			{:else}
+				<img src={image} alt={title} class="aspect-square w-full object-cover" />
+			{/if}
 			<span
 				class="bg-opacity-75 absolute inset-0 flex items-center justify-center bg-gray-100 p-4 text-center text-black opacity-0 transition-opacity group-hover:opacity-100"
 			>
